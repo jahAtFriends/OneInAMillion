786079272af8d5c9db3be993d0d6d6c7a29c09fa
jahAtFriends <jhammer@friendsbalt.org>
2021-09-14 12:47:28 -0400

    move examples to special dir

---
+++ Examples/Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    
+    //Print two arguments ordered smaller then larger.
+    public static void main(String[] args) {
+        
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        
+        //If a and b are in the wrong order, swap them.
+        if () {
+            
+        }
+        
+        System.out.println(a + " < " + b);
+    }
+}
